07f0d439f6585d7c88a30e9b753653beaf9701c0
albert.zah <albert.zah@antgroup.com>
2023-08-14 10:55:44 +0800

    Use jax tree util instead.

--- setup_secretflow.py
+++ setup_secretflow.py
@@ -40,7 +40,7 @@ class CleanCommand(setuptools.Command):
 
 setup(
     name='secretflow-rayfed',
-    version='0.2.0a3',
+    version='0.2.0a7',
     license='Apache 2.0',
     description='A multiple parties joint, distributed execution engine based on Ray,'
                 'to help build your own federated learning frameworks in minutes.',
